9e00090ee9f5186d513501f92075da7cf3ee54be
bbangjun23 <99010394+bbangjun23@users.noreply.github.com>
2022-02-16 16:51:10 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+$('.wrap').fullpage({
+    navigation:true,
+    anchors:['1page','2page','3page','4page','5page','6page']
+});
+
+$('.img1').colorbox({rel:'img1'});
+$('.youtube').colorbox({iframe:true, innerWidth:1200, innerHeight:720, rel:'img1'})
